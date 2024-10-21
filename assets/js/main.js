47e5fa14a47a5dd7d0ee9f00147f69a056889b67
tonnyOnyango <tonyonyango23@gmail.com>
2024-10-21 16:38:55 +0300

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+// Get the button element
+const javaclass = document.getElementById("colorButton");
+
+// Add event listener for the button click
+javaclass.addEventListener('click', () => {
+    javaclass.style.backgroundColor = javaclass.style.backgroundColor === 'red' ? '4CAF50' : 'red';
+});
